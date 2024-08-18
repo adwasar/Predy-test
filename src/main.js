a6c41a2946c4915582e11ac180bfdedc1b426975
Vladislav <104913853+Adwasar@users.noreply.github.com>
2024-08-18 16:23:33 +0300

    feat: primary slider

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
 import * as bootstrap from 'bootstrap'
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
 
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 import './scss/main.scss'
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.custom-slider__pagination',
+  },
+  navigation: {
+    nextEl: '.custom-slider__btn-next',
+    prevEl: '.custom-slider__btn-prev',
+  },
+  breakpoints: {
+    575: {
+      slidesPerView: 2,
+    },
+    767: {
+      slidesPerView: 3,
+    },
+    1199: {
+      slidesPerView: 4,
+    },
+  },
+})
